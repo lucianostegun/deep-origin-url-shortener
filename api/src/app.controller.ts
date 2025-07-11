25ba87df470ffc26fe4e53382027406c12a9abbd
Luciano Stegun <lucianostegun@gmail.com>
2025-07-11 14:48:04 -0300

    INITIAL COMMIT - API setup

---
+++ api/src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): object {
+    return {
+      message: 'Welcome to DeepOrigin URL Shortener API!',
+      version: '1.0.0',
+    };
+  }
+}
