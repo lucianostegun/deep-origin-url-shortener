72925d1e54bba4f32e18b211e9099ae52c5bc165
Luciano Stegun <lucianostegun@gmail.com>
2025-07-14 18:11:19 -0300

    Configure to run api and app on docker

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Accept connections from any host
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://api:3000', // Use Docker service name for API
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
---
+++ docker/api/start-container.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /application
+npm install && npm run start:dev
